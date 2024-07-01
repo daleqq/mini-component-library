9d501abd8443a45dc2061c694b3c92fc5a970c8f
daleqq <daleqq@gmail.com>
2024-07-01 12:03:12 +0800

    feat: complete input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,43 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Wrapper = styled.label`
+  font-size: var(--fontSize);
+  color: ${COLORS.gray700};
+  position: relative;
+  display: block;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Input = styled.input`
+  height: ${24 / 16}rem;
+  border: none;
+  padding-left: 24px;
+  font-weight: 700;
+  color: inherit;
+  width: var(--width);
+  border-bottom: var(--borderBottom);
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 500;
+  }
+`;
+
+const STYLES = {
+  small: {
+    '--borderBottom': '1px solid black',
+    '--fontSize': '14px'
+  },
+  large: {
+    '--borderBottom': '2px solid black',
+    '--fontSize': '18px'
+  }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +50,26 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const currentStyle = STYLES[size];
+  
+  return (
+    <Wrapper style={currentStyle}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{'--size': 16 + 'px'}}>
+        <Icon id={icon} size={16} />
+      </IconWrapper>
+      <Input placeholder={placeholder} style={{'--width': width + 'px'}} />
+    </Wrapper>
+  );
 };
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+  width: var(--size);
+`;
 
 export default IconInput;
